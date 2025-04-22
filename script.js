c94bdddbd608a2cef5d6b8c40d706c8063993837
mu0p <ryangranich@gmail.com>
2025-04-22 11:01:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const enterBtn = document.getElementById('enter-btn');
+const overlay = document.getElementById('enter-overlay');
+const content = document.getElementById('main-content');
 
+enterBtn.addEventListener('click', () => {
+  overlay.style.display = 'none';
+  content.style.display = 'block';
+
+  const audio = new Audio('images/9d9_Audio.wav');
+  audio.play();
+});
